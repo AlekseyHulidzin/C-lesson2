9fc8de828a14f5cb1934e9f75d4106c35e4fdb43
Aleksey Gulidin <89095500+AlekseyHulidzin@users.noreply.github.com>
2022-09-21 22:47:59 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1  */
+
+
+Console.WriteLine("Введите трехзначное число число");
+string namberStr = Console.ReadLine();
+int namber_lenght = namberStr.ToString().Length;//   длина чисел
+int namber = int.Parse(namberStr);
+
+int average_number = ((namber / 10) % 10);
+
+
+if (namber_lenght > 3 || namber_lenght < 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine($"{average_number} - Это среднее значение");
+}
+
